7bf567d1cb29b2fb52e3deabca9873c13ef1ae9b
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-06-07 20:09:02 +0545

    form completed

--- lib/screens/detection/analysis_save_form_screen.dart
+++ lib/screens/detection/analysis_save_form_screen.dart
@@ -15,7 +15,9 @@ class PredictedAnalysisSaveFormScreen extends StatefulWidget {
 
 class _PredictedAnalysisSaveFormScreenState
     extends State<PredictedAnalysisSaveFormScreen> {
+  bool addToPreviousOptions = false;
   int? selectedCategory;
+  int selectedBody = 0; // 0 represents front body, 1 represents back body
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +26,7 @@ class _PredictedAnalysisSaveFormScreenState
       appBar: CoreWidgets.appBarWidget(
         screenContext: context,
         hasBackButton: false,
-        titleWidget: const Text('Save Data'),
+        titleWidget: const Text(''),
       ),
       body: CustomScaffoldBodyWidget2(
         child: SingleChildScrollView(
@@ -81,6 +83,90 @@ class _PredictedAnalysisSaveFormScreenState
                             ],
                           ),
                           AppGaps.hGap24,
+                          Row(
+                            children: [
+                              Expanded(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      selectedBody = 0;
+                                    });
+                                  },
+                                  child: Container(
+                                    height: 100,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      color: selectedBody == 0
+                                          ? AppColors.primaryColor
+                                          : Colors.transparent,
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/front.png',
+                                          height: 80,
+                                          width: 80,
+                                        ),
+                                        const SizedBox(width: 8),
+                                        Expanded(
+                                          child: Text(
+                                            'Front',
+                                            style: TextStyle(
+                                              color: selectedBody == 0
+                                                  ? AppColors.white
+                                                  : AppColors.black,
+                                              fontSize: 17,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 8),
+                              Expanded(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      selectedBody = 1;
+                                    });
+                                  },
+                                  child: Container(
+                                    height: 100,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(8),
+                                      color: selectedBody == 1
+                                          ? AppColors.primaryColor
+                                          : Colors.transparent,
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        Image.asset(
+                                          'assets/back.png',
+                                          height: 80,
+                                          width: 80,
+                                        ),
+                                        const SizedBox(width: 8),
+                                        Expanded(
+                                          child: Text(
+                                            'Back',
+                                            style: TextStyle(
+                                              color: selectedBody == 1
+                                                  ? AppColors.white
+                                                  : AppColors.black,
+                                              fontSize: 17,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          AppGaps.hGap24,
                           CustomTextFormField(
                             isReadOnly: true,
                             labelText: 'Select Category',
@@ -92,17 +178,19 @@ class _PredictedAnalysisSaveFormScreenState
                               child: CustomIconButtonWidget(
                                 fixedSize: const Size(20, 20),
                                 child: SvgPicture.asset(
-                                    AppAssetImages.arrowDownSVGLogoLine,
-                                    height: 12,
-                                    color: AppColors.primaryColor),
+                                  AppAssetImages.arrowDownSVGLogoLine,
+                                  height: 12,
+                                  color: AppColors.primaryColor,
+                                ),
                               ),
                               itemBuilder: (context) => [
                                 PopupMenuItem<int>(
                                   value: 0,
                                   child: const Text(
                                     'Head',
-                                    style:
-                                        TextStyle(color: AppColors.primaryColor),
+                                    style: TextStyle(
+                                      color: AppColors.primaryColor,
+                                    ),
                                     textAlign: TextAlign.center,
                                   ),
                                   onTap: () {
@@ -115,8 +203,9 @@ class _PredictedAnalysisSaveFormScreenState
                                   value: 1,
                                   child: const Text(
                                     'Body',
-                                    style:
-                                        TextStyle(color: AppColors.primaryColor),
+                                    style: TextStyle(
+                                      color: AppColors.primaryColor,
+                                    ),
                                     textAlign: TextAlign.center,
                                   ),
                                   onTap: () {
@@ -129,8 +218,9 @@ class _PredictedAnalysisSaveFormScreenState
                                   value: 2,
                                   child: const Text(
                                     'Right Arm',
-                                    style:
-                                        TextStyle(color: AppColors.primaryColor),
+                                    style: TextStyle(
+                                      color: AppColors.primaryColor,
+                                    ),
                                     textAlign: TextAlign.center,
                                   ),
                                   onTap: () {
@@ -143,8 +233,9 @@ class _PredictedAnalysisSaveFormScreenState
                                   value: 3,
                                   child: const Text(
                                     'Left Arm',
-                                    style:
-                                        TextStyle(color: AppColors.primaryColor),
+                                    style: TextStyle(
+                                      color: AppColors.primaryColor,
+                                    ),
                                     textAlign: TextAlign.center,
                                   ),
                                   onTap: () {
@@ -157,8 +248,9 @@ class _PredictedAnalysisSaveFormScreenState
                                   value: 4,
                                   child: const Text(
                                     'Right Leg',
-                                    style:
-                                        TextStyle(color: AppColors.primaryColor),
+                                    style: TextStyle(
+                                      color: AppColors.primaryColor,
+                                    ),
                                     textAlign: TextAlign.center,
                                   ),
                                   onTap: () {
@@ -171,8 +263,9 @@ class _PredictedAnalysisSaveFormScreenState
                                   value: 5,
                                   child: const Text(
                                     'Left Leg',
-                                    style:
-                                        TextStyle(color: AppColors.primaryColor),
+                                    style: TextStyle(
+                                      color: AppColors.primaryColor,
+                                    ),
                                     textAlign: TextAlign.center,
                                   ),
                                   onTap: () {
@@ -183,6 +276,79 @@ class _PredictedAnalysisSaveFormScreenState
                                 ),
                               ],
                             ),
+                          ),
+                          AppGaps.hGap15,
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Radio<int>(
+                                    value: 0,
+                                    groupValue: addToPreviousOptions ? null : 0,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        addToPreviousOptions = false;
+                                      });
+                                    },
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const [
+                                      Text(
+                                        'New Result',
+                                        style: TextStyle(
+                                          color: AppColors.black,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Save object as a new result to the category',
+                                        style: TextStyle(
+                                          color: AppColors.bodyTextColor,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Radio<int>(
+                                    value: 1,
+                                    groupValue: addToPreviousOptions ? 1 : null,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        addToPreviousOptions = true;
+                                      });
+                                    },
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: const [
+                                      Text(
+                                        'Add to Previous',
+                                        style: TextStyle(
+                                          color: AppColors.black,
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Save object to the existing result',
+                                        style: TextStyle(
+                                          color: AppColors.bodyTextColor,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                           AppGaps.hGap24,
                           CustomTextFormField(
@@ -209,18 +375,22 @@ class _PredictedAnalysisSaveFormScreenState
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              CustomStretchedButtonWidget6(
+              CustomStretchedButtonWidget(
                 onTap: () {
                   Navigator.pushNamed(
-                      context, AppPageNames.homeNavigatorScreen);
+                    context,
+                    AppPageNames.homeNavigatorScreen,
+                  );
                 },
                 child: const Text('Save Result'),
               ),
               AppGaps.hGap10,
-              CustomStretchedButtonWidget5(
+              CustomStretchedButtonWidget4(
                 onTap: () {
                   Navigator.pushNamed(
-                      context, AppPageNames.homeNavigatorScreen);
+                    context,
+                    AppPageNames.homeNavigatorScreen,
+                  );
                 },
                 child: const Text('Cancel'),
               ),
@@ -232,25 +402,21 @@ class _PredictedAnalysisSaveFormScreenState
   }
 
   String getCategoryText(int? category) {
-    if (category == null) {
-      return 'Select a Category';
-    } else {
-      switch (category) {
-        case 0:
-          return 'Head';
-        case 1:
-          return 'Body';
-        case 2:
-          return 'Right Arm';
-        case 3:
-          return 'Left Arm';
-        case 4:
-          return 'Right Leg';
-        case 5:
-          return 'Left Leg';
-        default:
-          return 'Select a Category';
-      }
+    switch (category) {
+      case 0:
+        return 'Head';
+      case 1:
+        return 'Body';
+      case 2:
+        return 'Right Arm';
+      case 3:
+        return 'Left Arm';
+      case 4:
+        return 'Right Leg';
+      case 5:
+        return 'Left Leg';
+      default:
+        return 'Head';
     }
   }
 }

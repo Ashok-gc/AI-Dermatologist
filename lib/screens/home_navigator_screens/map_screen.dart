257aded2936f61d1368b6a7d9083ab284331b094
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-06-09 00:59:00 +0545

    map screen

--- lib/screens/home_navigator_screens/map_screen.dart
+++ lib/screens/home_navigator_screens/map_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 
+import '../../utils/constants/app_constants.dart';
 import '../../widgets/core_widgets.dart';
 
 class MapScreen extends StatefulWidget {
@@ -11,17 +12,56 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
-  /// Width of small screen size
   @override
   Widget build(BuildContext context) {
-    /// Get screen size
     return Scaffold(
+      backgroundColor: AppColors.primaryColor,
       /* <-------- Appbar --------> */
       appBar: CoreWidgets.appBarWidget(
-          screenContext: context,
-          hasBackButton: false,
-          titleWidget: const Text('My Map')),
+        screenContext: context,
+        hasBackButton: false,
+        titleWidget: const Text(''),
+      ),
       /* <-------- Content --------> */
+      body: CustomScaffoldBodyWidget2(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // AppGaps.hGap10,
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: AppGaps.screenPaddingValue,
+                ),
+                child: Text(
+                  'Nearest Hospitals',
+                  style: TextStyle(
+                    color: AppColors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            AppGaps.hGap15,
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: AppGaps.screenPaddingValue,
+                ),
+                decoration: const BoxDecoration(
+                  color: AppColors.container,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(28)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
